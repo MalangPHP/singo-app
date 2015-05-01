4126cbdf95e9d9b20bb37384e30e5c6c33074931
Prasetyo Wicaksono <jowy@oiry.net>
2015-05-01 13:09:54 +0700

    fix doctrine cli

--- cli-config.php
+++ cli-config.php
@@ -2,8 +2,20 @@
 
 require "vendor/autoload.php";
 
-$app = new \Singo\Application();
+$app = new \Singo\Application(
+    [
+        "app.path" => __DIR__ . "/src",
+        "app.public.path" => __DIR__,
+        "config.path" => __DIR__ . "/src/Config/config.yml",
+        "config.cache.lifetime" => 300,
+        "cache.driver" => "array",
+        "cache.options" => [
+            "namespace" => "singo",
+        ],
+        "use.module" => true,
+    ]
+);
 
-$app->init();
+$app->init($app);
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($app["orm.em"]);
